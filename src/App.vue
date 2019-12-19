75061673199a111f7adccee7ba102990b67ca207
Mikhail Sidorov <7780462+mikhailsidorov@users.noreply.github.com>
2019-12-19 16:02:08 +0300

    Create app structure

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,21 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
+    <Layout>
+      <router-view />
+    </Layout>
   </div>
 </template>
+
+<script>
+import Layout from './components/Layout'
+
+export default {
+  name: 'App',
+  components: {
+    Layout
+  }
+}
+</script>
 
 <style lang="scss">
 #app {
@@ -15,18 +24,5 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
 }
 </style>
---
+++ src/views/Books.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="books">Books</div>
+</template>
+
+<script>
+export default {
+  name: 'Books'
+}
+</script>
+
+<style></style>
