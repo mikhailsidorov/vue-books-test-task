93c807b05784e2d6a72a092334c6db4ef3db7f40
Mikhail Sidorov <7780462+mikhailsidorov@users.noreply.github.com>
2019-12-22 22:39:21 +0300

    Extract books table row

--- src/components/BooksTable/index.vue
+++ src/components/BooksTable/index.vue
@@ -1,29 +1,19 @@
 <template>
   <div class="books-table">
     <BooksTableHeader />
-    <div v-for="book in books" :key="book.id" class="books-table__row">
-      <div class="books-table__cell">{{ book.title }}</div>
-      <div class="books-table__cell">{{ book.author.firstName + ' ' + book.author.lastName }}</div>
-      <div class="books-table__cell">{{ book.numberOfPages }}</div>
-      <div class="books-table__cell">{{ book.publishingHouse }}</div>
-      <div class="books-table__cell">{{ book.publishingYear }}</div>
-      <div class="books-table__cell">{{ book.releaseDate }}</div>
-      <div class="books-table__cell">
-        <figure class="book-image-wrapper">
-          <img class="book-image" :src="book.image" />
-        </figure>
-      </div>
-    </div>
+    <BooksTableRow v-for="book in books" :key="book.id" :book="book" />
   </div>
 </template>
 
 <script>
-import BooksTableHeader from '../BookTableHeader'
+import BooksTableHeader from '../BooksTableHeader'
+import BooksTableRow from '../BooksTableRow'
 
 export default {
   name: 'BooksTable',
   components: {
-    BooksTableHeader
+    BooksTableHeader,
+    BooksTableRow
   },
   props: {
     books: {
@@ -40,26 +30,5 @@ export default {
   margin: 0 auto;
   display: table;
   text-align: left;
-
-  &__row {
-    display: table-row;
-  }
-
-  &__cell {
-    padding: 10px 10px 10px 0;
-    display: table-cell;
-    vertical-align: middle;
-  }
-}
-
-.book-image-wrapper {
-  width: 40px;
-  height: 40px;
-  margin: 0;
-}
-
-.book-image {
-  width: 100%;
-  height: 100%;
 }
 </style>
---
+++ src/components/BooksTableRow/index.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="books-table-row">
+    <div class="books-table-cell">{{ book.title }}</div>
+    <div class="books-table-cell">{{ book.author.firstName + ' ' + book.author.lastName }}</div>
+    <div class="books-table-cell">{{ book.numberOfPages }}</div>
+    <div class="books-table-cell">{{ book.publishingHouse }}</div>
+    <div class="books-table-cell">{{ book.publishingYear }}</div>
+    <div class="books-table-cell">{{ book.releaseDate }}</div>
+    <div class="books-table-cell">
+      <figure class="book-image-wrapper">
+        <img class="book-image" :src="book.image" />
+      </figure>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BooksTableRow',
+  props: {
+    book: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
+
+<style>
+.books-table-row {
+  display: table-row;
+}
+
+.books-table-cell {
+  padding: 10px 10px 10px 0;
+  display: table-cell;
+  vertical-align: middle;
+}
+
+.book-image-wrapper {
+  width: 40px;
+  height: 40px;
+  margin: 0;
+}
+
+.book-image {
+  width: 100%;
+  height: 100%;
+}
+</style>
