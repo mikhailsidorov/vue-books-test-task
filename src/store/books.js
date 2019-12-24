b21c4d7cdf5a21524b915debfa8c03f95d9cbcf7
Mikhail Sidorov <7780462+mikhailsidorov@users.noreply.github.com>
2019-12-24 17:39:29 +0300

    Add saving to localStorage

--- src/store/books.js
+++ src/store/books.js
@@ -1,46 +1,6 @@
 export default {
   state: {
-    list: [
-      {
-        id: 1,
-        title: 'Book 1',
-        author: {
-          firstName: 'John',
-          lastName: 'Doe'
-        },
-        numberOfPages: 163,
-        publishingHouse: 'O`Relly',
-        publishingYear: 2029,
-        releaseDate: '2029-10-10',
-        image: 'https://via.placeholder.com/300x450'
-      },
-      {
-        id: 2,
-        title: 'Book 2',
-        author: {
-          firstName: 'John',
-          lastName: 'Doe'
-        },
-        numberOfPages: 153,
-        publishingHouse: 'O`Relly',
-        publishingYear: 2010,
-        releaseDate: '2010-10-10',
-        image: 'https://via.placeholder.com/300x450'
-      },
-      {
-        id: 3,
-        title: 'Book 3',
-        author: {
-          firstName: 'John',
-          lastName: 'Doe'
-        },
-        numberOfPages: 153,
-        publishingHouse: 'O`Relly',
-        publishingYear: 2012,
-        releaseDate: '2012-10-10',
-        image: 'https://via.placeholder.com/300x450'
-      }
-    ]
+    list: []
   },
   actions: {},
   mutations: {
